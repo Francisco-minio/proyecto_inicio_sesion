7a34b4c07c91c6b4b0d653a1cb3178116dc39968
Francisco-minio <84881490+Francisco-minio@users.noreply.github.com>
2021-07-19 19:43:41 -0400

    proyecto ruby

---
+++ app/models/usuario.rb
@@ -0,0 +1,5 @@
+class Usuario < ApplicationRecord
+    EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+    validates :nombre, :apellido, presence: true, length: { in: 2..20 }
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+  end
